1b708419dde8d9acf8b0383250be59be9fc1b4de
saeed-developer <saeednajafimedia@gmail.com>
2022-01-21 00:33:27 +0330

    execute pull

--- mega/Deployment.js
+++ mega/Deployment.js
@@ -1,10 +1,14 @@
 const fastify = require('fastify')({logger : true})
+const {promisify}= require('util');
+const exec = promisify(require('child_process').exec);
+promisify(exec)
 const {writeFile} = require('fs/promises')
 require('dotenv').config({path : './config/.env'})
 fastify.post('/master' , async(request, reply)=>{
 let header  =  JSON.stringify(request.headers)
-let body = JSON.stringify(request.body )
+let body = JSON.stringify(request.body)
 await writeFile('./file.txt' ,` header : ${header} + \n body :  ${body} \n`, {flag : 'a+'}) 
+await exec('git pull')
 reply.send(null)
 })
 const start = async(port)=>{
