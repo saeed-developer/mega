72f8919962e42c07b70dcdff43d2cbebce0cfcbb
saeed-developer <saeednajafimedia@gmail.com>
2022-01-21 00:57:27 +0330

    new lib for git

--- mega/Deployment.js
+++ mega/Deployment.js
@@ -1,15 +1,17 @@
 const fastify = require('fastify')({logger : true})
-const {promisify}= require('util');
-const exec = promisify(require('child_process').exec);
-promisify(exec)
+//const {promisify}= require('util');
+//const exec = promisify(require('child_process').exec);
+const simpleGit = require("simple-git");
+const git = simpleGit.default();
+//promisify(exec)
 const {writeFile} = require('fs/promises')
 require('dotenv').config({path : './config/.env'})
 fastify.post('/master' , async(request, reply)=>{
 let header  =  JSON.stringify(request.headers)
 let body = JSON.stringify(request.body)
 await writeFile('./file.txt' ,` header : ${header} + \n body :  ${body} \n`, {flag : 'a+'}) 
-await exec('git pull')
-reply.send(null)
+await git.pull()
+reply.send(null) 
 })
 const start = async(port)=>{
     try { 
