e0ba54464b601acd6a0fd66b3db1d1dd77dde524
saeed-developer <saeednajafimedia@gmail.com>
2022-01-21 00:47:35 +0330

    test

--- mega/Authentication.js
+++ mega/Authentication.js
@@ -1,6 +1,6 @@
 const fastify = require('fastify')({ logger: true })
 fastify.get('/', async (request, reply) => {
-  reply.send('dorod')
+  reply.send('update')
 })
 require('dotenv').config({path : './config/.env'})
 const start = async (port) => {
