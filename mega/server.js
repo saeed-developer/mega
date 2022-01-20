3f19c6afe5502a60f38e369c571576a7eb2d2a7b
Saeed <81457933+saeed-developer@users.noreply.github.com>
2022-01-20 15:53:17 +0330

    Add files via upload

---
+++ mega/server.js
@@ -0,0 +1,13 @@
+const fastify = require('fastify')({ logger: true })
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
+const start = async () => {
+  try {
+    await fastify.listen(3001)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
