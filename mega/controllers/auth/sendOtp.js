73ebaa19d3f1d02f1a3e47cba9cbd264fdd18a88
saeed-developer <saeednajafimedia@gmail.com>
2022-01-21 20:42:31 +0330

    clean console

--- mega/controllers/auth/sendOtp.js
+++ mega/controllers/auth/sendOtp.js
@@ -6,11 +6,9 @@ module.exports.sendOtp =  async function  (request , reply){
     else {
        let otp = '';
        for(let number = 0 ;number < 6 ; number++){ 
-       const digit =Math.floor(Math.random() * 10);
-       console.log(otp) 
+       const digit =Math.floor(Math.random() * 10); 
        otp += String(digit)  
-       }    
-       console.log(String(request.body.number))  
+       } 
   const farazsms = await axios.post('http://ippanel.com/api/select',{ 
     "op" : "send",
     "uname" : "09374613195",
@@ -18,10 +16,9 @@ module.exports.sendOtp =  async function  (request , reply){
     "message" : `کدتایید شما :‌${otp}`,  
     "from": "3000505",
     "to" : [String(request.body.number)],     
-  }) 
-  console.log('farazsms ======>',farazsms.status)
+  })
+  if(farazsms.status === 200){
+    console.log('farazsms ======>',farazsms.status)
   await this.redis.set(request.body.number, otp , "EX",180 )           
-  reply.send({message : 'sms have sent'})         
-    }
-   
- }  
+  reply.send({message : 'sms has sent'})  
+  }}}  
