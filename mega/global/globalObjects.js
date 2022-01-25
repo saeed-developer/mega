9a8cf1e42e77a086f4d1ef90b92929dbe304d3d3
saeed-developer <saeednajafimedia@gmail.com>
2022-01-25 22:44:53 +0330

    no logs

--- mega/global/globalObjects.js
+++ mega/global/globalObjects.js
@@ -2,5 +2,5 @@ module.exports.logger = {
     translateTime: 'SYS:HH:MM:ss Z',
     singleLine : 'true',
     ignore: 'pid,hostname,reqId,responseTime,res,req',
-    messageFormat: '{req.method} {req.url} {req} {res.statusCode} '
+    messageFormat: '{req.method} {req.url} {hostname} {res.statusCode} {req.hostname}'
   } 
