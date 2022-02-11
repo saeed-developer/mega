98bdf59bb6dc46611f5e5f8a5f9757e5f2ecbb62
saeed-developer <saeednajafimedia@gmail.com>
2022-02-11 22:20:16 +0330

    config

--- mega/ecosystem.config.js
+++ mega/ecosystem.config.js
@@ -1,17 +1,17 @@
 module.exports = {
   apps : [{
     name   : "View",
-    script : "cd services/View.js",
+    script : "services/View.js",
     watch: true
   },
 {
 name : "Auth",
-script : "cd services/Authentication.js",
+script : "services/Authentication.js",
 watch : true,
 },
 {
 name : "Deploy",
-script : "cd services/Deployment.js",
+script : "services/Deployment.js",
 watch : true
 }
 
