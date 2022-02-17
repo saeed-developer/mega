cfd7b5965f140be516c0c79c9c1a4739713cf11b
saeed-developer <saeednajafimedia@gmail.com>
2022-02-17 20:29:03 +0330

    send and verify otp changed;

--- mega/controllers/auth/sendOtp.js
+++ mega/controllers/auth/sendOtp.js
@@ -1,19 +1,51 @@
-const axios = require('axios')
-module.exports.sendOtp =  async function  (request , reply){ 
-       let otp = '';
-       for(let number = 0 ;number < 6 ; number++){ 
-       const digit =Math.floor(Math.random() * 10); 
-       otp += String(digit)  
-       } 
-  const farazsms = await axios.post('http://ippanel.com/api/select',{ 
-    "op" : "send",
-    "uname" : "09374613195",
-    "pass":  "faraz3242056663", 
-    "message" : `کدتایید شما در پیوی چنل: ${otp} \n زمان ارسال : ${new Date(Date.now()).toString().slice(16 ,24)}`,  
-    "from": "3000505",
-    "to" : [String(request.body.number)],     
-  })
-  if(farazsms.status === 200){
-  await this.redis.set(request.body.number, otp , "EX",180 )           
-  reply.send({message : 'sms has sent'})  
-  }}  
+const axios = require("axios");
+const User = require("../../models/User");
+module.exports.sendOtp = async function (request, reply) {
+  const { number } = request.body;
+  const user = new User();
+  const data = await user.findByPhone(number);
+  if (data.length > 0) {
+    let otp = "";
+    for (let number = 0; number < 6; number++) {
+      const digit = Math.floor(Math.random() * 10);
+      otp += String(digit);
+    }
+    const farazsms = await axios.post("http://ippanel.com/api/select", {
+      op: "send",
+      uname: "09374613195",
+      pass: "faraz3242056663",
+      message: `کدتایید شما در پیوی چنل: ${otp} \n زمان ارسال : ${new Date(
+        Date.now()
+      )
+        .toString()
+        .slice(16, 24)}`,
+      from: "3000505",
+      to: [String(request.body.number)],
+    });
+    if (farazsms.status === 200) {
+      await this.redis.hmset(
+        request.body.number,
+        "otp",
+        otp,
+        "username",
+        data[0].username,
+        "id",
+        data[0].id
+      );
+      await this.redis.expire(request.body.number, 180);
+      reply.send({ message: "sms has sent" });
+    } else {
+      reply.code(503).send({
+        message: {
+          persian: "اس ام اس ارسال نشد",
+        },
+      });
+    }
+  } else {
+    reply.code(404).send({
+      message: {
+        persian: "کاربری با این شماره تلفن پیدا نشد",
+      },
+    });
+  }
+};
--- mega/controllers/auth/verifyOtp.js
+++ mega/controllers/auth/verifyOtp.js
@@ -1,25 +1,28 @@
-const mysql = require("mysql");
 module.exports.verifyOtp = async function (request, reply) {
   const { number, code } = request.body;
-  const otp = await this.redis.get(number);
-  console.log(code, otp);
-  if (String(otp) === String(code)) {
+  const data = await this.redis.hgetall(number);
+
+  if (String(data.otp) === String(code)) {
     const access = await this.jwt.sign(
       {
-        username: username,
-        id: data[0].id,
+        username: data.username,
+        id: data.id,
       },
       { expiresIn: "1h" }
     );
     const refresh = await this.jwt.sign(
       {
-        username: username,
-        id: data[0].id,
+        username: data.username,
+        id: data.id,
       },
       { key: process.env.JWT_REFRESH, expiresIn: "7 days" }
     );
     reply.send({ access: access, refresh: refresh });
   } else {
-    reply.status(401).send({ message: "not ok" });
+    reply.status(401).send({
+      message: {
+        persian: "کد تایید اشتباه است",
+      },
+    });
   }
 };
