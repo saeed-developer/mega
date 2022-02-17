f57e36934b616b9a16263be66b10bfe6698e4b8e
saeed-developer <saeednajafimedia@gmail.com>
2022-02-17 19:09:13 +0330

    'verify'

--- mega/controllers/auth/verifyOtp.js
+++ mega/controllers/auth/verifyOtp.js
@@ -1,13 +1,25 @@
-const mysql = require('mysql');
-module.exports.verifyOtp =async function (request , reply){
-    const {number , code} = request.body
-    const otp = await this.redis.get(number)
-    console.log(code , otp )
-    if(String(otp) === String(code)){
-         reply.send({message : 'ok'})
-    }
-    else {
-    reply.status(401).send({message : 'not ok'})
-    }
-   
-}
+const mysql = require("mysql");
+module.exports.verifyOtp = async function (request, reply) {
+  const { number, code } = request.body;
+  const otp = await this.redis.get(number);
+  console.log(code, otp);
+  if (String(otp) === String(code)) {
+    const access = await this.jwt.sign(
+      {
+        username: username,
+        id: data[0].id,
+      },
+      { expiresIn: "1h" }
+    );
+    const refresh = await this.jwt.sign(
+      {
+        username: username,
+        id: data[0].id,
+      },
+      { key: process.env.JWT_REFRESH, expiresIn: "7 days" }
+    );
+    reply.send({ access: access, refresh: refresh });
+  } else {
+    reply.status(401).send({ message: "not ok" });
+  }
+};
