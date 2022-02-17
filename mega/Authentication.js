5c576c5c59a1f522174a2324b52892254caa4dd5
saeed-developer <saeednajafimedia@gmail.com>
2022-02-17 18:59:22 +0330

    igonore yarn log,jwt tokens

--- mega/Authentication.js
+++ mega/Authentication.js
@@ -1,58 +1,68 @@
-require('dotenv').config({path : './config/.env'})
-const { sendOtp } = require('./controllers/auth/sendOtp')
-const { logger } = require('./global/globalObjects');
-const fastify = require('fastify')({
-  logger: process.env.ENVIRONMENT=== 'development' ?{
-       prettyPrint:logger  
-     }:false
- })
-const {sendOtpSchema, VerifyOtpSchema, signupScheme, loginSchema} = require('./controllers/auth/authSchema');  
-const { verifyOtp } = require('./controllers/auth/verifyOtp');
-const {pool} = require('./config/db');
-const User = require('./models/User');
-const { signup } = require('./controllers/auth/signup');
-const { login } = require('./controllers/auth/login');
-fastify.register(require('fastify-redis'), { host: '127.0.0.1' })
-const {redis} = fastify 
-  const query =  (async ()=>{
-    try {      
-      const createTable = await pool.execute(`
+require("dotenv").config({ path: "./config/.env" });
+const { sendOtp } = require("./controllers/auth/sendOtp");
+const { logger } = require("./global/globalObjects");
+const jwt = require("fastify-jwt");
+const fastify = require("fastify")({
+  logger:
+    process.env.ENVIRONMENT === "development"
+      ? {
+          prettyPrint: logger,
+        }
+      : false,
+});
+fastify.register(jwt, {
+  secret: process.env.JWT_SECRET,
+});
+const {
+  sendOtpSchema,
+  VerifyOtpSchema,
+  signupScheme,
+  loginSchema,
+} = require("./controllers/auth/authSchema");
+const { verifyOtp } = require("./controllers/auth/verifyOtp");
+const { pool } = require("./config/db");
+const User = require("./models/User");
+const { signup } = require("./controllers/auth/signup");
+const { login } = require("./controllers/auth/login");
+fastify.register(require("fastify-redis"), { host: "127.0.0.1" });
+const { redis } = fastify;
+const query = (async () => {
+  try {
+    const createTable = await pool.execute(`
       CREATE TABLE IF NOT EXISTS  users (id INT AUTO_INCREMENT PRIMARY KEY NOT NULL UNIQUE,
       phone BIGINT UNIQUE ,username VARCHAR(255) UNIQUE NOT NULL, password VARCHAR(255) NOT NULL,
       first_name VARCHAR(255),last_name  VARCHAR(255) , 
-      created_at DATETIME DEFAULT CURRENT_TIMESTAMP)`) 
-    }  
-    catch(err){
-      throw err       
-    }
-     
-   })()
-fastify.post('/mobile/check',{
-  schema : sendOtpSchema,          
-  handler : sendOtp    
-}) 
-fastify.post('/mobile/verify', {
-  shcema:VerifyOtpSchema,
-  handler:verifyOtp
-}) 
-fastify.post('/login' , {
-  schema : loginSchema,
-  handler : login  
-})
-fastify.post('/signup',{      
-  schema :signupScheme,
-  handler : signup
-})
+      created_at DATETIME DEFAULT CURRENT_TIMESTAMP)`);
+  } catch (err) {
+    throw err;
+  }
+})();
+fastify.post("/mobile/check", {
+  schema: sendOtpSchema,
+  handler: sendOtp,
+});
+fastify.post("/mobile/verify", {
+  shcema: VerifyOtpSchema,
+  handler: verifyOtp,
+});
+fastify.post("/login", {
+  schema: loginSchema,
+  handler: login,
+});
+fastify.post("/signup", {
+  schema: signupScheme,
+  handler: signup,
+});
 //fastify.post('/refresh')
-  
-require('dotenv').config({path : './config/.env'})
+
+require("dotenv").config({ path: "./config/.env" });
 const start = async (port) => {
   try {
-    await fastify.listen(port) 
-    console.log(`auth port ${port} started`)
+    await fastify.listen(port);
+    console.log(`auth port ${port} started`);
   } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)      
+    fastify.log.error(err);
+    process.exit(1);
   }
-}
-start(process.env.AUTH_PORT) ;      
+};
+start(process.env.AUTH_PORT);
--- mega/controllers/auth/login.js
+++ mega/controllers/auth/login.js
@@ -1,23 +1,36 @@
-const User = require("../../models/User")
-const bcrypt = require('bcrypt');
-module.exports.login = async function(request , reply) {
-const {username , password} = request.body
-const user = new User()
-try {
-const data = await user.findByUsername(username)
-const checkPass = await bcrypt.compare(password , data[0].password)
-if(checkPass){
-reply.send({message : 'ok'})
-}
-else {
-    reply.code(401).send({message : {
-        english : 'your username or password wrong',
-        persian : 'نام کاربری یا گذرواژه شما اشتباه است'
-
-    }})
-}
-}
-catch (err){
-    throw err
-}
-}
+const User = require("../../models/User");
+const bcrypt = require("bcrypt");
+module.exports.login = async function (request, reply) {
+  const { username, password } = request.body;
+  const user = new User();
+  try {
+    const data = await user.findByUsername(username);
+    const checkPass = await bcrypt.compare(password, data[0].password);
+    if (checkPass) {
+      const access = await this.jwt.sign(
+        {
+          username: username,
+          id: data[0].id,
+        },
+        { expiresIn: "1h" }
+      );
+      const refresh = await this.jwt.sign(
+        {
+          username: username,
+          id: data[0].id,
+        },
+        { key: process.env.JWT_REFRESH, expiresIn: "7 days" }
+      );
+      reply.send({ access: access, refresh: refresh });
+    } else {
+      reply.code(401).send({
+        message: {
+          english: "your username or password wrong",
+          persian: "نام کاربری یا گذرواژه شما اشتباه است",
+        },
+      });
+    }
+  } catch (err) {
+    throw err;
+  }
+};
