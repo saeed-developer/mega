94ad4ef6249f64c0b969d92f1b0c14fb31d4095a
saeed-developer <saeednajafimedia@gmail.com>
2022-03-07 20:59:21 +0330

    config pm2 chat added

--- mega/ecosystem.config.js
+++ mega/ecosystem.config.js
@@ -1,19 +1,24 @@
 module.exports = {
-  apps : [{
-    name   : "View",
-    script : "View.js",
-    watch: true
-  },
-{
-name : "Auth",
-script : "Authentication.js",
-watch : true,
-},
-{
-name : "Deploy",
-script : "Deployment.js",
-watch : true
-}
-
-]
-}
+  apps: [
+    {
+      name: "View",
+      script: "View.js",
+      watch: true,
+    },
+    {
+      name: "Auth",
+      script: "Authentication.js",
+      watch: true,
+    },
+    {
+      name: "Deploy",
+      script: "Deployment.js",
+      watch: true,
+    },
+    {
+      name: "Chat",
+      script: "Chat.js",
+      watch: true,
+    },
+  ],
+};
